e1deaebe41aaf228c34ec1364f0cea87ba4e8980
zhu.zeng <zengzhu.cn@gmail.com>
2022-02-16 01:59:17 +0800

    cleaning: remove the sample codes.

--- modules/ex-log-advisor-spring-boot-autoconfigure/src/main/java/net/thiki/ec/component/log/autoconfigure/ExceptionConfig.java
+++ modules/ex-log-advisor-spring-boot-autoconfigure/src/main/java/net/thiki/ec/component/log/autoconfigure/ExceptionConfig.java
@@ -1,12 +1,6 @@
 package net.thiki.ec.component.log.autoconfigure;
 
-import net.thiki.ec.component.log.autoconfigure.AssertionExceptionHandler;
-import net.thiki.ec.component.log.starter.Greeter;
-import net.thiki.ec.component.log.starter.GreetingConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.ControllerAdvice;
