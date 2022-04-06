3bd3aa02f5d5e18874787841062d5d50e8e42751
zhu.zeng <zhu.zeng@xuncetech.com>
2022-04-06 16:05:59 +0800

    generate codes with StringWriter

--- modules/code-generators/src/main/java/net/thiki/ec/component/code/generator/EntityWrapperProxyGenerator.kt
+++ modules/code-generators/src/main/java/net/thiki/ec/component/code/generator/EntityWrapperProxyGenerator.kt
@@ -1,12 +1,52 @@
 package net.thiki.ec.component.code.generator
 
+import freemarker.cache.ClassTemplateLoader
+import freemarker.template.Configuration
+import freemarker.template.Template
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
+import java.io.StringWriter
+
+
 /**
  *
  */
-class EntityWrapperProxyGenerator {
+class EntityWrapperProxyGenerator(
+    private val templatePath: String? = null,
+    private val templateName: String? = null,
+    private val outputRoot: String? = null
+) {
+
+
 
     fun <T> generateJavaCode(entity: EntityClassMeta<T>): String{
-        return "";
+
+        // freemarker 配置
+        val configuration = Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS)
+
+        configuration.defaultEncoding = "UTF-8"
+        val defaultDir = "/net/thiki/ec/component/code/generator"
+        configuration.templateLoader = ClassTemplateLoader(
+            javaClass,
+            templatePath ?: defaultDir
+        )
+
+        // 根据模板名称获取路径下的模板
+        val template: Template = configuration.getTemplate(templateName ?: "entity-class.ftl")
+        // 处理路径问题
+
+
+        // 定义一个输出流来导出代码文件
+//        val outputStreamWriter = OutputStreamWriter(FileOutputStream(entity.calOutFileName(outputRoot ?: "c:$defaultDir/generated")))
+        val stringWriter = StringWriter()
+        // freemarker 引擎将动态数据绑定的模板并导出为文件
+
+        template.process(entity, stringWriter)
+
+//        template.process(entity, outputStreamWriter)
+//        outputStreamWriter.flush()
+
+        return stringWriter.toString()
     }
 
     fun generateJavaCode(classGroup: ClassGroup): String{
@@ -24,11 +64,24 @@ data class ClassGroup(
 
 /**
  * 某个Entity
+ * [clazz] : 某个需要代理的类
+ * [includedFields] : 如果为空，则所有public的getter setter都包含在内；否则只尝试包含[includedFields]内的字段
+ * [excludedFields] : 如果不为空，则排除[excludedFields]内的字段，其他所有public的getter setter都包含在内
  */
 data class EntityClassMeta<T>(
     val clazz: Class<T>,
-    val fields: Map<String, FieldMeta<*>> = emptyMap()
-)
+    val includedFields: Map<String, FieldMeta<*>> = emptyMap(),
+    val excludedFields: Map<String, FieldMeta<*>> = emptyMap(),
+){
+    fun calOutFileName(root: String): String{
+        val clazzName = clazz.simpleName
+       return "$root/$clazzName$EXT"
+    }
+
+    companion object{
+        const val EXT = ".java"
+    }
+}
 
 /**
  *
--- modules/code-generators/src/test/java/net/thiki/ec/component/code/generator/EntityWrapperProxyGeneratorTest.kt
+++ modules/code-generators/src/test/java/net/thiki/ec/component/code/generator/EntityWrapperProxyGeneratorTest.kt
@@ -27,61 +27,70 @@ internal class EntityWrapperProxyGeneratorTest{
     )
 
     val javaCode =
-"""
-    class PersonDo extends Person{
+"""class PersonDo extends Person{
 
-        private final Person person;
+    private final Person person;
 
-        public PersonDo(Person person){
-            this.person = person;
-        }
-
-        /* delegate codes begin */
-        @Override
-        public String getFirstName() {
-            return this.person.getFirstName();
-        }
-
-        @Override
-        public String getLastName() {
-            return this.person.getLastName();
-        }
-
-        @Override
-        public int getAge() {
-            return this.person.getAge();
-        }
-
-        @Override
-        public void setFirstName(String firstName) {
-            this.person.setFirstName(firstName);
-        }
-
-        @Override
-        public void setLastName(String lastName) {
-            this.person.setLastName(lastName);
-        }
-
-        @Override
-        public void setAge(int age) {
-            this.person.setAge(age);
-        }
-
-        /* delegate codes end */
-
-        /* TODO: write your business logic specified to your application.  */
-
+    public PersonDo(Person person){
+        this.person = person;
     }
-""".trimIndent()
+
+    /* delegate codes begin */
+    @Override
+    public String getFirstName() {
+        return this.person.getFirstName();
+    }
+
+    @Override
+    public String getLastName() {
+        return this.person.getLastName();
+    }
+
+    @Override
+    public int getAge() {
+        return this.person.getAge();
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        this.person.setFirstName(firstName);
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.person.setLastName(lastName);
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.person.setAge(age);
+    }
+
+    /* delegate codes end */
+
+    /* TODO: write your business logic specified to your application.  */
+
+}""".trimIndent()
 
     @Test
     @Disabled("not implemented")
     fun testEntityProxyGenerator(){
         println(javaCode);
-        val cut = EntityWrapperProxyGenerator();
+        val cut = EntityWrapperProxyGenerator()
         val generatedJavaCode = cut.generateJavaCode(EntityClassMeta(Person::class.java))
 
-        assertEquals(javaCode, generatedJavaCode)
+        assertEquals(
+            javaCode.replace("\r\n", "\n"),
+            generatedJavaCode.replace("\r\n", "\n")
+        )
 
+    }
+
+    @Test
+    fun testStringBuffer(){
+        val cut = StringBuffer();
+        cut.append(3);
+        cut.append(",hello world")
+        assertEquals("3,hello world", cut.toString())
     }
 }
