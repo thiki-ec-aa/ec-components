7130da03f32e378f087e5f752f0c4795914247f0
zhu.zeng <zengzhu.cn@gmail.com>
2022-04-07 11:19:51 +0800

    toDataModel

--- modules/code-generators/src/main/java/net/thiki/ec/component/code/generator/EntityWrapperProxyGenerator.kt
+++ modules/code-generators/src/main/java/net/thiki/ec/component/code/generator/EntityWrapperProxyGenerator.kt
@@ -3,9 +3,10 @@ package net.thiki.ec.component.code.generator
 import freemarker.cache.ClassTemplateLoader
 import freemarker.template.Configuration
 import freemarker.template.Template
-import java.io.FileOutputStream
-import java.io.OutputStreamWriter
+import net.thiki.ec.component.exception.assertThat
 import java.io.StringWriter
+import java.lang.Character.isUpperCase
+import java.lang.reflect.Field
 
 
 /**
@@ -41,7 +42,7 @@ class EntityWrapperProxyGenerator(
         val stringWriter = StringWriter()
         // freemarker 引擎将动态数据绑定的模板并导出为文件
 
-        template.process(entity, stringWriter)
+        template.process(entity.toDataModel(), stringWriter)
 
 //        template.process(entity, outputStreamWriter)
 //        outputStreamWriter.flush()
@@ -70,23 +71,95 @@ data class ClassGroup(
  */
 data class EntityClassMeta<T>(
     val clazz: Class<T>,
-    val includedFields: Map<String, FieldMeta<*>> = emptyMap(),
-    val excludedFields: Map<String, FieldMeta<*>> = emptyMap(),
+    val includedFields: Set<String> = emptySet(),
+    val excludedFields: Set<String> = emptySet(),
+    val targetPackage: String? = clazz.`package`.name
 ){
     fun calOutFileName(root: String): String{
         val clazzName = clazz.simpleName
        return "$root/$clazzName$EXT"
     }
 
+    /**
+     * 生成template使用的DataModel
+     */
+    fun toDataModel(): Map<String, Any>{
+        val filteredFields = getAllFields()
+        val imports = filteredFields.values.map {
+            "import ${it.javaClass.canonicalName}"
+        }.sorted()
+
+        val fields = filteredFields.map {(fieldName, fieldType) ->
+            fieldName to fieldType
+        }.toMap()
+
+        return mapOf(
+            "package" to targetPackage!!,
+            "className" to clazz.simpleName!!,
+            "imports" to imports,
+            "fields" to fields
+        )
+    }
+
+    /**
+     * 按javabean规范，从public方法里筛出成对的getter和setter，作为一个field，输出其名字和类型
+     */
+    private fun getAllFields(): MutableMap<String, Class<*>> {
+//        return clazz.fields.filter { hasField(it) }
+        val methodMap = clazz.methods.associateBy { it.name }
+
+        val map = mutableMapOf<String, Class<*>>()
+        for (method in clazz.methods) {
+            val methodName = method.name
+            if (methodName.startsWith("get") || methodName.startsWith("is")){
+                val fieldName = extractFieldNameFromGetter(methodName)
+                if (methodMap.containsKey(buildSetterName(fieldName))){
+                    map[fieldName] = method.returnType
+                }
+            }
+        }
+        return map
+    }
+
+    private fun buildSetterName(fieldName: String): String {
+        TODO("Not yet implemented")
+    }
+
+    private fun extractFieldNameFromGetter(getterName: String): String {
+//        if (getterName)
+        assertThat(getterName.isNotEmpty())
+        val first = getterName[0]
+        if (getterName.length == 1){
+            return first.lowercase()
+        }
+        val second = getterName[1]
+
+        if (isUpperCase(first) && isUpperCase(second)){
+            return ""
+        }
+
+        return getterName.substring(2, 3).lowercase() + getterName.substring(3, getterName.length)
+    }
+
+    private fun isGetterOrSetter(name: String): Boolean {
+        return name.startsWith("get")
+                || name.startsWith("set")
+                || name.startsWith("is")
+    }
+
+    private fun hasField(field: Field): Boolean {
+        return if (excludedFields.contains(field.name)) {
+            false
+        } else {
+            if (includedFields.isEmpty()) {
+                true
+            } else {
+                includedFields.contains(field.name)
+            }
+        }
+    }
+
     companion object{
         const val EXT = ".java"
     }
 }
-
-/**
- *
- */
-data class FieldMeta<T>(
-    val clazz: Class<T>
-)
-
