db94452e7320357d93f2f97654eb53cccef90380
zhu.zeng <zengzhu.cn@gmail.com>
2022-05-06 00:55:23 +0800

    log ex

--- modules/ex-log-advisor-spring-boot-autoconfigure/src/main/java/net/thiki/ec/component/log/autoconfigure/AssertionExceptionHandler.java
+++ modules/ex-log-advisor-spring-boot-autoconfigure/src/main/java/net/thiki/ec/component/log/autoconfigure/AssertionExceptionHandler.java
@@ -2,6 +2,7 @@ package net.thiki.ec.component.log.autoconfigure;
 
 import net.thiki.ec.component.exception.AssertionException;
 import net.thiki.ec.component.exception.FailureResponseBody;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -39,8 +40,7 @@ public class AssertionExceptionHandler {
                 //ignored
             }
             if (logger.isDebugEnabled()){
-                logger.debug(re.getMessage());
-                logger.debug(Arrays.toString(re.getStackTrace()));
+                logger.debug(ex2String(re));
             }
             return ResponseEntity.status(httpStatus)
                     .body(new FailureResponseBody<>(String.valueOf(ae.getCode()), ae.getMessage(), ae.getBizParams()))
@@ -48,13 +48,29 @@ public class AssertionExceptionHandler {
         } else {
             // other runtime exceptions
             if (logger.isDebugEnabled()){
-                logger.debug(re.getMessage());
-                logger.debug(Arrays.toString(re.getStackTrace()));
+                logger.debug(ex2String(re));
             }
             return ResponseEntity.status(httpStatus)
                     .body(new FailureResponseBody<>(String.valueOf(AssertionException.CodeUnknown), re.getMessage(), re))
                     ;
         }
+    }
+
+    @NotNull
+    private String ex2String(Throwable re) {
+        StringBuilder sb = new StringBuilder();
+        sb
+                .append("Exception thrown: \n")
+                .append("message:")
+                .append(re.getMessage())
+                .append("\nstacktrace:\n");
+        for (StackTraceElement e: re.getStackTrace()) {
+            sb
+                    .append("at ")
+                    .append(e.toString())
+                    .append("\n");
+        }
+        return sb.toString();
     }
 
 }
