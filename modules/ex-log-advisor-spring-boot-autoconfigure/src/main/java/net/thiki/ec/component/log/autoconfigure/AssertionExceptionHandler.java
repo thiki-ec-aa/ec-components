658d60f87f0de278900415c0468d7c5375561121
zhu.zeng <zhu.zeng@xuncetech.com>
2022-05-05 22:30:58 +0800

    log the exception

--- modules/ex-log-advisor-spring-boot-autoconfigure/src/main/java/net/thiki/ec/component/log/autoconfigure/AssertionExceptionHandler.java
+++ modules/ex-log-advisor-spring-boot-autoconfigure/src/main/java/net/thiki/ec/component/log/autoconfigure/AssertionExceptionHandler.java
@@ -2,15 +2,20 @@ package net.thiki.ec.component.log.autoconfigure;
 
 import net.thiki.ec.component.exception.AssertionException;
 import net.thiki.ec.component.exception.FailureResponseBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+
 @ControllerAdvice
 public class AssertionExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(AssertionExceptionHandler.class);
     /**
      *
      * @param re
@@ -33,11 +38,19 @@ public class AssertionExceptionHandler {
             } else {
                 //ignored
             }
+            if (logger.isDebugEnabled()){
+                logger.debug(re.getMessage());
+                logger.debug(Arrays.toString(re.getStackTrace()));
+            }
             return ResponseEntity.status(httpStatus)
                     .body(new FailureResponseBody<>(String.valueOf(ae.getCode()), ae.getMessage(), ae.getBizParams()))
                     ;
         } else {
             // other runtime exceptions
+            if (logger.isDebugEnabled()){
+                logger.debug(re.getMessage());
+                logger.debug(Arrays.toString(re.getStackTrace()));
+            }
             return ResponseEntity.status(httpStatus)
                     .body(new FailureResponseBody<>(String.valueOf(AssertionException.CodeUnknown), re.getMessage(), re))
                     ;
